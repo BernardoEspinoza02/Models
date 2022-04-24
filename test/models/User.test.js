76fd737a94d527900506f4fe709c4184711d8a77
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-24 14:21:54 -0500

    Requerimiento 4: Objetos creado con setters, codigos modificador y agregada prueba 3

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -15,7 +15,7 @@ describe("Unit tests for User class", () => {
         expect(user.dateCreated).not.toBeU//Verifica que el valor no sea undefine
         expect(user.dateCreated).not.toBeUndefined ()
         expect(user.lastUpdated).not.toBeUndefined ()
-        })
+    });
 
     test('2 Add getters', () => {
         const user = new User(1, "bernardo", "Carlo", "Bio")
@@ -24,7 +24,16 @@ describe("Unit tests for User class", () => {
         expect(user.getBio).toBe("Bio")
         expect(user.getDateCreated).not.toBeUndefined ()
         expect(user.getLastUpdated).not.toBeUndefined ()
-        });
+    });
+
+    test ('3 Add setters', () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        user.setUsername = "Gilmar"
+        expect(user.username).toBe("Gilmar")
+
+        user.setBio = "New bio"
+        expect(user.bio).toBe("New bio")
+    });
 }) 
 
 
