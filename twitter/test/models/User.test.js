3bcf02d062a8b5750fe6dab35707725a116780a2
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-24 01:56:29 -0500

    Agregando package.json, modificando user.test.js

---
+++ twitter/test/models/User.test.js
@@ -0,0 +1,18 @@
+const User = require ('./../models/user')
+
+describe("Unit tests for User class", () => {
+
+    test('Create an User object', () => {
+        // Aqui invocas el codigo que vas a usar en tu app
+        const user = new User(1, "Carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+
+        //Aqui validas los resultados de ese codigo 
+        // Este es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("Carlogilmar")
+        expect(user.name).toBe("Carlo")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dataCreated")
+        expect(user.lastUpdated).toBe("lastUpdate")
+    })
+})
