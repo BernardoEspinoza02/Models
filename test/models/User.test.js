e16b2042cbf31659885dfd314302b833fa88a37a
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-24 13:53:25 -0500

    requerimento 2, Propiedades 'DataCreated' y ' lastUpdated'

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,7 +4,7 @@ describe("Unit tests for User class", () => {
 
     test('Create an User object', () => {
         // Aqui invocas el codigo que vas a usar en tu app
-        const user = new User(1, "Carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "Carlogilmar", "Carlo", "Bio")
 
         //Aqui validas los resultados de ese codigo 
         // Este es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
@@ -12,9 +12,10 @@ describe("Unit tests for User class", () => {
         expect(user.username).toBe("Carlogilmar")
         expect(user.name).toBe("Carlo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
-    })
+        expect(user.dateCreated).not.toBeU//Verifica que el valor no sea undefine
+        expect(user.dateCreated).not.toBeUndefined ()
+        expect(user.lastUpdated).not.toBeUndefined ()
+        });
 }) 
 
 
